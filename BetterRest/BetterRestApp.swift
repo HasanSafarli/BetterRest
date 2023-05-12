334d468393c37b0a2ede3291037c7ddb06bad3d0
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-05-12 22:51:54 +0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hasan Safarli on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
